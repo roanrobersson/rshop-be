737d80f4baf077b7d2dc07737c3fc888d06a3438
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2021-09-09 23:06:52 -0300

    Fix N+1 queries

--- src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -36,6 +36,7 @@ public class ProductService {
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
 		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		Page<Product> page = repository.find(categories, name, pageRequest);
+		repository.findProductsWithCategories(page.getContent());
 		return page.map(x -> new ProductDTO(x));
 	}
 	
