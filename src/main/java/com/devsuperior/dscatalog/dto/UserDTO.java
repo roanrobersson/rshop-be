e30f9d2b0d8c6072e06c16f11b8208eadc86d20d
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2021-09-09 22:52:49 -0300

    Basic beans validation

--- src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,14 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotEmpty(message = "Campo obrigatório")
 	private String firstName;
+	
+	@NotEmpty(message = "Campo obrigatório")
 	private String lastName;
+	
+	@Email(message = "Favor entrar um email válido")
 	private String email;
 	
 	private Set<RoleDTO> roles = new HashSet<>();
