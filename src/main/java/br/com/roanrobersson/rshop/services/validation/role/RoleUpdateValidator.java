268361ab51165d6feee3785a5ce83b308e815208
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2021-11-23 23:06:06 -0300

    Fix exception when update role without change authority text

--- src/main/java/br/com/roanrobersson/rshop/services/validation/role/RoleUpdateValidator.java
+++ src/main/java/br/com/roanrobersson/rshop/services/validation/role/RoleUpdateValidator.java
@@ -2,11 +2,14 @@ package br.com.roanrobersson.rshop.services.validation.role;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import br.com.roanrobersson.rshop.controllers.exceptions.FieldMessage;
 import br.com.roanrobersson.rshop.dto.role.RoleUpdateDTO;
@@ -14,6 +17,9 @@ import br.com.roanrobersson.rshop.entities.Role;
 import br.com.roanrobersson.rshop.repositories.RoleRepository;
 
 public class RoleUpdateValidator implements ConstraintValidator<RoleUpdateValid, RoleUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private RoleRepository repository;
@@ -25,10 +31,15 @@ public class RoleUpdateValidator implements ConstraintValidator<RoleUpdateValid,
 	@Override
 	public boolean isValid(RoleUpdateDTO dto, ConstraintValidatorContext context) {
 		
+		@SuppressWarnings("unchecked")
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		String roleId = uriVars.get("id");
+		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		Role role = repository.findByAuthority(dto.getAuthority());
-		if (role != null) {
+
+		if (role != null && !roleId.equals(role.getId())) {
 			list.add(new FieldMessage("authority", "Role already exists"));
 		}
 		
