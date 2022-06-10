84e5c575301fed1535c4ffe27290c42f148fb43e
Roan de Oliveira <roanrobersson@gmail.com>
2022-06-10 17:53:05 -0300

    chore: configure TestContainers to be optionally

--- src/test/java/br/com/roanrobersson/rshop/integration/IT.java
+++ src/test/java/br/com/roanrobersson/rshop/integration/IT.java
@@ -1,8 +1,12 @@
 package br.com.roanrobersson.rshop.integration;
 
+import java.io.IOException;
+
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -11,15 +15,22 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public abstract class IT {
 
-	protected static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.29")
-			.withDatabaseName("rshop_test").withUsername("any").withPassword("any");
+	private static final String DATABASE_NAME = "rshop_test";
+	private static final String USER_NAME = "any";
+	private static final String PASSWORD = "any";
 
-	// Override application.it.properties
-//	@DynamicPropertySource
-//	static void mySQLProperties(DynamicPropertyRegistry registry) {
-//		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-//		registry.add("spring.datasource.password", mySQLContainer::getPassword);
-//		registry.add("spring.datasource.username", mySQLContainer::getUsername);
-//	}
+	@SuppressWarnings("resource")
+	@DynamicPropertySource
+	private static void checkRunWithTestcontainers(DynamicPropertyRegistry registry) throws IOException {
+		Boolean enabled = Boolean.valueOf(System.getenv().get("RSHOP_TESTCONTAINERS_ENABLED"));
 
+		if (enabled) {
+			MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.29").withDatabaseName(DATABASE_NAME)
+					.withUsername(USER_NAME).withPassword(PASSWORD);
+			mySQLContainer.start();
+			registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+			registry.add("spring.datasource.password", mySQLContainer::getPassword);
+			registry.add("spring.datasource.username", mySQLContainer::getUsername);
+		}
+	}
 }
